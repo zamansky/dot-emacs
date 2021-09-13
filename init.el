c8fa75ff59a92530286722149601c5b15595c548
Mike Zamansky <zamansky@gmail.com>
2021-09-13 16:45:17 -0400

    z

--- init.el
+++ init.el
@@ -59,3 +59,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
