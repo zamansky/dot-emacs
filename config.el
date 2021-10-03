95c6973b4ca8dde06f05b57ed807f0f10e15372c
Mike Zamansky <zamansky@gmail.com>
2021-10-02 20:57:54 -0400

    switched selectrum to vertico

--- config.el
+++ config.el
@@ -87,30 +87,69 @@
 ;; exec-path-from-shell:1 ends here
 
 ;; [[file:config.org::*Completion frameworks][Completion frameworks:1]]
-(use-package selectrum
+;; (use-package selectrum
+    ;; :init
+    ;; (selectrum-mode +1))
+
+  (use-package vertico
+    :init
+    (vertico-mode +1))
+
+  (use-package orderless
   :init
-  (selectrum-mode +1))
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
 
-(use-package consult
-  :general
-  ("M-y" 'consult-yank))
-
-(setq completion-ignore-case t)
-(setq read-file-name-completion-ignore-case t)
-
-(use-package marginalia
-  :config (marginalia-mode))
-
-
-(use-package orderless
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
   :init
-  (setq completion-styles '(orderless)))
+  (savehist-mode))
 
-(use-package company
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 3)
-  (global-company-mode t))
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; Alternatively try `consult-completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+
+
+  (use-package consult
+    :general
+    ("M-y" 'consult-yank))
+
+  (setq completion-ignore-case t)
+  (setq read-file-name-completion-ignore-case t)
+
+  (use-package marginalia
+    :config (marginalia-mode))
+
+
+  (use-package orderless
+    :init
+    (setq completion-styles '(orderless)))
+
+  (use-package company
+    :config
+    (setq company-idle-delay 0)
+    (setq company-minimum-prefix-length 3)
+    (global-company-mode t))
 ;; Completion frameworks:1 ends here
 
 ;; [[file:config.org::*org][org:1]]
