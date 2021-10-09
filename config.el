a52d4aee37aebb1aebaf7d62e2ca0a94a806821b
Mike Zamansky <zamansky@gmail.com>
2021-10-09 06:52:08 -0400

    cleaned up vertico stuff to bare bones

--- config.el
+++ config.el
@@ -95,7 +95,7 @@
     :init
     (vertico-mode +1))
 
-  (use-package orderless
+(use-package orderless
   :init
   (setq completion-styles '(orderless)
         completion-category-defaults nil
@@ -106,27 +106,7 @@
   :init
   (savehist-mode))
 
-;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; Alternatively try `consult-completing-read-multiple'.
-  (defun crm-indicator (args)
-    (cons (concat "[CRM] " (car args)) (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
 
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-  ;; Vertico commands are hidden in normal buffers.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
   (use-package marginalia
     :config (marginalia-mode))
 
