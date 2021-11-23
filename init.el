0af48538b16666020ed58c9bc42e15bacc8a5b0e
Mike Zamansky <zamansky@gmail.com>
2021-11-23 16:33:52 -0500

    added gnuplot

--- init.el
+++ init.el
@@ -37,22 +37,87 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#5f5f5f" "#ff4b4b" "#a1db00" "#fce94f" "#5fafd7" "#d18aff" "#afd7ff" "#ffffff"])
+ '(awesome-tray-mode-line-active-color "#0031a9")
+ '(awesome-tray-mode-line-inactive-color "#d7d7d7")
+ '(custom-safe-themes
+   '("583148e87f779040b5349db48b6fcad6fe9a873c6ada20487e9a1ec40d845505" "0feb7052df6cfc1733c1087d3876c26c66410e5f1337b039be44cb406b6187c6" "27a1dd6378f3782a593cc83e108a35c2b93e5ecc3bd9057313e1d88462701fcd" "f703efe04a108fcd4ad104e045b391c706035bce0314a30d72fbf0840b355c2c" default))
+ '(exwm-floating-border-color "#888888")
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-themes-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-themes-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-themes-fringe-yellow))
+ '(highlight-tail-colors '(("#aecf90" . 0) ("#c0efff" . 20)))
+ '(hl-sexp-background-color "#efebe9")
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#70480f")
+     ("TODO" . "#721045")
+     ("NEXT" . "#5317ac")
+     ("THEM" . "#8f0075")
+     ("PROG" . "#00538b")
+     ("OKAY" . "#30517f")
+     ("DONT" . "#315b00")
+     ("FAIL" . "#a60000")
+     ("BUG" . "#a60000")
+     ("DONE" . "#005e00")
+     ("NOTE" . "#863927")
+     ("KLUDGE" . "#813e00")
+     ("HACK" . "#813e00")
+     ("TEMP" . "#5f0000")
+     ("FIXME" . "#a0132f")
+     ("XXX+" . "#972500")
+     ("REVIEW" . "#005a5f")
+     ("DEPRECATED" . "#201f55")))
+ '(ibuffer-deletion-face 'modus-themes-mark-del)
+ '(ibuffer-filter-group-name-face 'modus-themes-pseudo-header)
+ '(ibuffer-marked-face 'modus-themes-mark-sel)
+ '(ibuffer-title-face 'default)
  '(org-confirm-babel-evaluate nil)
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/Sync/orgfiles")
  '(org-export-html-postamble nil)
  '(org-export-with-toc nil)
  '(org-hide-leading-stars t)
+ '(org-src-block-faces 'nil)
  '(org-src-fontify-natively t)
  '(org-startup-folded 'overview)
  '(org-startup-indented t)
+ '(pdf-view-midnight-colors '("#000000" . "#f8f8f8"))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
 	   (lambda nil
 	     (if
 		 (y-or-n-p "Tangle?")
 		 (org-babel-tangle)))
-	   nil t))))
+	   nil t)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#a60000")
+     (40 . "#721045")
+     (60 . "#8f0075")
+     (80 . "#972500")
+     (100 . "#813e00")
+     (120 . "#70480f")
+     (140 . "#5d3026")
+     (160 . "#184034")
+     (180 . "#005e00")
+     (200 . "#315b00")
+     (220 . "#005a5f")
+     (240 . "#30517f")
+     (260 . "#00538b")
+     (280 . "#093060")
+     (300 . "#0031a9")
+     (320 . "#2544bb")
+     (340 . "#0000c0")
+     (360 . "#5317ac")))
+ '(vc-annotate-very-old-color nil)
+ '(xterm-color-names
+   ["black" "#a60000" "#005e00" "#813e00" "#0031a9" "#721045" "#00538b" "gray65"])
+ '(xterm-color-names-bright
+   ["gray35" "#972500" "#315b00" "#70480f" "#2544bb" "#8f0075" "#30517f" "white"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
