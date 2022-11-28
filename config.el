af2bb5b2ec8591b547e4e05f8f17cae1dca5de2b
Mike Zamansky <zamansky@gmail.com>
2022-11-28 15:32:43 -0500

    added projectile

--- config.el
+++ config.el
@@ -487,7 +487,7 @@
 ;; [[file:config.org::*Proectile][Proectile:1]]
 (use-package projectile)
 (projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 ;; Proectile:1 ends here
 
