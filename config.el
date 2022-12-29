b961d9df2b3e17d73b8775e4968f582abdfabc31
Mike Zamansky <zamansky@gmail.com>
2022-12-29 07:34:05 -0500

    z

--- config.el
+++ config.el
@@ -571,15 +571,14 @@
              ("C-c ." . cider-reset-test-run-tests))
       )
 
-  (defun my-clojure-mode-hook ()
-      ;;(clj-refactor-mode 1)
+    (defun my-clojure-mode-hook ()
+      (clj-refactor-mode 1)
       (yas-minor-mode 1) ; for adding require/use/import statements
       ;; This choice of keybinding leaves cider-macroexpand-1 unbound
       (cljr-add-keybindings-with-prefix "C-c C-m"))
-  ;; (use-package clj-refactor
-  ;; :ensure t
-  ;; :config
-  ;; (add-hook 'clojure-mode-hook #'my-clojure-mode-hook))
+  (use-package clj-refactor
+    :config
+  (add-hook 'clojure-mode-hook #'my-clojure-mode-hook))
 ;; Clojure:1 ends here
 
 ;; [[file:config.org::*Rust][Rust:1]]
